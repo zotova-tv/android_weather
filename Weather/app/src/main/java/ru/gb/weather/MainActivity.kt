601592a02795852f15a19948db3c937eb0b533b8
Tatiana Zotova <zotova-tv@outlook.com>
2022-03-09 23:46:01 +0300

    Lesson6

--- Weather/app/src/main/java/ru/gb/weather/MainActivity.kt
+++ Weather/app/src/main/java/ru/gb/weather/MainActivity.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
 import ru.gb.weather.R
 
 class MainActivity : AppCompatActivity() {
